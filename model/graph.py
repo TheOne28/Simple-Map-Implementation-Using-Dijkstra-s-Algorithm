9e98530fe115b0e0cd1e4c87a7b6b872fc82447d
Vincent Atmadja <vincentpatmadja@gmail.com>
2022-06-03 22:47:55 +0700

    Init djikstra function

--- model/graph.py
+++ model/graph.py
@@ -3,12 +3,39 @@ class Graph:
         self.node = [i for i in range (1, node + 1)]
         self.adjMatrix = [[-1 for i in range(len(node))] for j in range (len(node))]
     
-    def addWeight(self, src : int, dest: int, weight: int):
+    def addWeight(self, src : int, dest: int, weight: int) -> None:
         if(src not in self.node or dest not in self.node):
             raise("Node tidak ada")
 
         self.adjMatrix[src][dest] = weight
         
-
-    def djikstr(self, start, dest):
+    def updateDistance(self, node : int) -> None:
         pass
+    
+    def getMinimum(self, node, visited : list) -> tuple(int, int):
+        toCheck = self.adjMatrix[node]
+
+        minimum = -1
+        indMin = -1
+        for i in range(len(toCheck)):
+            if(minimum == -1):
+                if((i + 1) not in visited):
+                    minimum = toCheck[i]
+                    indMin = i
+            else:
+                #Asumsi kalau misalnya ada node yang punya weight sama, dipilih nomor node terkecil
+                if((i + 1) not in visited and toCheck[i] < minimum):
+                    minimum = toCheck[i]
+                    indMin = i
+
+        return (minimum, indMin)
+                
+        
+
+    def djikstr(self, start, dest) -> None:
+        visited = []
+
+        current = start - 1
+        
+        while(current != dest - 1):
+            toCheck = self.adjMatrix[current]
